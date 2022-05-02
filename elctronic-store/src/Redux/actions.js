da0968cdc1a39305abfe5af9c8b0a22b70f9400a
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-05-02 15:12:36 +0530

    third commit

--- elctronic-store/src/Redux/actions.js
+++ elctronic-store/src/Redux/actions.js
@@ -1,3 +1,14 @@
+import { useState } from "react"
+
+const [data,setData]=useState([]);
 // action types
+export const ADD_ORDER="ADD_ORDER"
+
 
 // Action Creators
+export const add_order=()=>{
+    return{
+        type:"ADD_ORDER",
+        payload:data
+    }
+}
--- elctronic-store/src/Redux/reducer.js
+++ elctronic-store/src/Redux/reducer.js
@@ -1,10 +1,16 @@
+import { ADD_ORDER } from "./actions";
+
 const init = {
   orders:[],
-  
+  isLoggedIn:false,
+
 };
+
 
 export const reducer = (store = init, { type, payload }) => {
   switch (type) {
+    case ADD_ORDER:
+      return {...store,orders:[...store.orders+payload]}
     default:
       return store;
   }
