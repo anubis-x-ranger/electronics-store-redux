00594c63de19728b0fcdb60506b4e388350730a8
Satyajit Sahu <satyajitsahu123@outlook.com>
2022-05-02 16:14:32 +0530

    update

--- elctronic-store/src/Redux/actions.js
+++ elctronic-store/src/Redux/actions.js
@@ -1,17 +1,17 @@
 import { useState,useEffect } from "react"
 import { NewOrder } from "../components/NewOrder"
-
+import axios from "axios";
 // action types
 export const ADD_ORDER="ADD_ORDER"
+useEffect(()=>{
+    const {data}=await axios("http://localhost:8080/orders");
 
+},[]);
 
 // Action Creators
 export const add_order=()=>{
     return{
         type:"ADD_ORDER",
-        payload:{
-
-
-        }
+        payload:{data}
     }
 }
